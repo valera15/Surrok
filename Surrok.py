f7616d45c59a5c0889a1ad4d0928f4f7100fc51f
Leraron159 <144227244+valera15@users.noreply.github.com>
2024-05-10 19:37:22 +0300

    Версия 1.1
    тут запуск окна с закрытием на "Esc" и фото сурка

---
+++ Surrok.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+# "C:\Users\Leraron159\PycharmProjects\Surrok"
+win = tk.Tk() # окно
+win.title('Surrok')
+#win.geometry("1920x1200+0+0")
+win.geometry("960x600+1+0")
+#win.attributes('-fullscreen', True)
+win.resizable(False, False) # не меняется размер экрана
+win.wm_attributes("-topmost", 1) # приложение поверх всех окон
+win.bind("<Escape>", lambda event: win.destroy()) # event - событие, destroy - закрыть
+
+
+
+
+
+win.mainloop() # запуск окна (это в конце)
